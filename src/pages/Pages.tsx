a1161542805fdb591cd6b2cf15d43a151ce5bcb8
Raj Khatri <rjkhatri7@gmail.com>
2022-10-14 00:17:45 +0530

    fixes

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -8,7 +8,7 @@ import Searched from "./Searched";
 function Pages() {
   const location = useLocation();
   return (
-    <AnimatePresence exitBeforeEnter>
+    <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
         <Route path="/cuisine/:type" element={<Cuisine />} />
